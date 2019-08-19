59558b96d5325aeda7bb9c9c6eb7e9337712437e
Matheus <matheus24204@gmail.com>
2019-08-19 17:07:08 -0300

    Adicionar exercicios

---
+++ exercicio1.py
@@ -0,0 +1,6 @@
+num = int(input('Qual o número: '))
+
+if int(num) % 2 == 0:
+    print('O número informado é par ')
+else:
+    print('O número informado é impar ')
---
+++ exercicio2.py
@@ -0,0 +1,8 @@
+numero_celular = int(input("Digite um número de celular válido: "))
+
+if len(numero_celular) == 9 and numero_celular[0] == 9:
+    print("Número válido: ")
+
+else:
+    print('Número inválido: ')
+
